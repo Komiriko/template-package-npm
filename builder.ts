8b382394b94f49150fd88d2f2f1f41ed5288ef8d
Komiroko <komiriko@pm.me>
2025-07-01 09:25:43 +0200

    build(📦): [Update packages]

--- builder.ts
+++ builder.ts
@@ -8,7 +8,7 @@ await Bun.$`rm -rf dist`;
 console.log('🗑️  Deleted dist folder if it existed. ✅');
 
 await Bun.$`tsc --project tsconfig.build.json`;
-await Bun.$`tsc-alias -p tsconfig.build.json`;
+await Bun.$`bunx tsc-alias -p tsconfig.build.json`;
 console.log('🔍 Type analysis and generation completed. ✅');
 
 await Bun.build({
